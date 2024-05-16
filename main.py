bf28ee83117c1ba318b03670f2ed8aa7005f077c
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-05-16 18:46:11 +0300

    New commit, this guys change html code of their site, but you know... There is no safe code from me

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from dataclasses import dataclass
 import re
 import pandas as pd
 import bs4
+from constants import BASE_URL, BASE_URL_PY
 
-BASE_URL = "https://www.work.ua"
-BASE_URL_PY = "https://www.work.ua/jobs-python/"
 list_of_works = []
 list_of_vacs = []
 
@@ -42,30 +41,50 @@ async def get_vacs_all(list_of_works):
             async with session.get(work) as response:
                 html = await response.text()
                 soup = bs4.BeautifulSoup(html, "html.parser")
-                title = soup.find(id="h1-name").text
-                salary_paragraph = soup.select_one("span.strong-500")
-                salary = "Не вказана"
-                if salary_paragraph:
-                    salary_text = salary_paragraph.parent.text
-                    if re.search(r'\d', salary_text) and "–" in salary_text:
-                        matches = re.findall(r'\d+', salary_text)
-                        if matches:
-                            salary = ''.join(matches)
-                            if len(salary) % 2 == 0:
-                                salary = (salary[:(int(len(salary) / 2))] + " - " + salary[(int(len(salary) / 2)):])
-                            else:
-                                salary = (salary[:int(len(salary) / 2)] + " - " + salary[int(len(salary) / 2):])
-                city_paragraph = soup.select_one("li.text-indent.mt-sm span.glyphicon.glyphicon-map-marker.text-default.glyphicon-large")
-                if city_paragraph:
-                    city = city_paragraph.parent.text.split()[0]
-                    if "," in city:
-                        city = city[:-1]
-                skills = soup.select(".mt-2xl.flex.flex-wrap > ul > li > span")
-                skills_list = []
-                for skill in skills:
-                    skills_list.append(skill.text)
-                vac = Vacancy(title, city, salary, skills_list)
+                title = get_title(soup)
+                salary = get_salary(soup)
+                city = get_city(soup)
+                skills = get_skills(soup)
+                vac = Vacancy(title, city, salary, skills)
                 list_of_vacs.append(vac.__dict__)
+
+
+def get_title(soup):
+    return soup.find(id="h1-name").text
+
+
+def get_salary(soup):
+    salary_paragraph = soup.select_one("span.strong-500")
+    salary = "Не вказана"
+    if salary_paragraph:
+        salary_text = salary_paragraph.parent.text
+        if re.search(r'\d', salary_text) and "–" in salary_text:
+            matches = re.findall(r'\d+', salary_text)
+            if matches:
+                salary = ''.join(matches)
+                if len(salary) % 2 == 0:
+                    salary = (salary[:(int(len(salary) / 2))] + " - " + salary[(int(len(salary) / 2)):])
+                else:
+                    salary = (salary[:int(len(salary) / 2)] + " - " + salary[int(len(salary) / 2):])
+    return salary
+
+
+def get_city(soup):
+    city_paragraph = soup.select_one("li.text-indent.mt-sm span.glyphicon.glyphicon-map-marker.text-default.glyphicon-large")
+    city = "Не вказано"
+    if city_paragraph:
+        city = city_paragraph.parent.text.split()[0]
+        if "," in city:
+            city = city[:-1]
+    return city
+
+
+def get_skills(soup):
+    skills = soup.select(".mt-2xl.flex.flex-wrap > ul > li > span")
+    skills_list = []
+    for skill in skills:
+        skills_list.append(skill.text)
+    return skills_list
 
 
 async def main():
@@ -75,3 +94,4 @@ async def main():
     df.to_csv('vacancies.csv', index=False)
 
 asyncio.run(main())
+
